32314ea13cb801cdac072774126e1ab82fbdc80d
Jeppe Hauman <jeppe_hauman@hotmail.com>
2024-03-26 17:31:12 +0100

    first commit, includes everything from old portfolio

---
+++ projects.ts
@@ -0,0 +1,42 @@
+export const projects = [
+  {
+    title: "MasterChef Event",
+    description:
+      "Website for a new subcompany of MasterChef doing events at their studios. Built from the ground up using Sveltekit and Sanity CMS.",
+    img: "/src/images/masterchefevent.png",
+    tech: ["Svelte", "Sanity"],
+    url: "https://masterchefevent.dk",
+  },
+  {
+    title: "Newsmap",
+    description:
+      "Frontend containing an interactive worldmap giving you the latest news from the country you choose.",
+    img: "/src/images/News-Map.png",
+    tech: ["Vite", "React", "NewscatcherAPI"],
+    url: "https://newsmap.netlify.app/",
+  },
+  {
+    title: "Reis Celik",
+    description:
+      "A website made for the turkish film director Reis Celik, showing his previous work and awards.",
+    img: "/src/images/reis-celik.png",
+    tech: ["Nextjs", "Tailwindcss", "Typescript"],
+    url: "https://reis.vercel.app/",
+  },
+  {
+    title: "Leashes",
+    description:
+      "Fullstack application for an ecommerce selling handmade dog leashes. Site is still in development. TODOs: stripe payment & confirmation of purchase.",
+    img: "/src/images/Leashes.png",
+    tech: ["Nextjs", "Tailwindcss", "Jotai", "Typescript"],
+    url: "https://leashes.vercel.app/",
+  },
+  {
+    title: "Change Ahead",
+    description:
+      "A website made for the London-based NGO Change Ahead. An organisation dedicated to preventing homelessness and supporting those in need.",
+    img: "/src/images/Change-Ahead.png",
+    tech: ["Wordpress"],
+    url: "https://www.changeahead.org.uk/",
+  },
+];
